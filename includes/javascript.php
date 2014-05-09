5ba39954c58574c0a2b1e94a718363f23af1fac7
Ruby You <shih@svisualjoint.co.uk>
2014-05-09 03:08:47 +0100

    add form validation plugin & add body font

--- includes/javascript.php
+++ includes/javascript.php
@@ -3,16 +3,6 @@
 	<script src="scripts/jquery-1.11.0.js" type="text/javascript"></script>
 	<script src="scripts/bootstrap/bootstrap.js" type="text/javascript"></script>
 	<script src="scripts/jquery.accordion.source.js" type="text/javascript" charset="utf-8"></script>
+	<script src="scripts/jquery.validate.js" type="text/javascript" charset="utf-8"></script>
+	<script src="scripts/script.js" type="text/javascript" charset="utf-8"></script>
 	<script src="http://localhost:35729/livereload.js"></script>
-	<script type="text/javascript">
-		$(document).ready(function () {
-			$('.accordion').accordion();
-
-			$('.next').on("click",function(event){
-				$('#clientinfo').slideDown();
-				$('.delete').hide();
-				event.preventDefault();
-			});
-		});
-
-	</script>
