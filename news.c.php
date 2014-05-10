50eb54fd4056fcbbc77c7a50414f57265f2ae9b0
NickChen <nickchen14@gamil.com>
2014-05-11 02:04:57 +0800

    news categories done !!

--- news.c.php
+++ news.c.php
@@ -3,4 +3,6 @@ include_once 'config/config.main.front.php';
 
 $cNews = new News();
 $arr = $cNews->getNews_Front($_GET['i']);
+$cProduct = new Product();
+$categoriesMenu = $cProduct->getCategoriesMenu();
 ?>
