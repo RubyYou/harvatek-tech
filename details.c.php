aa2e53d33459a5d277d4f07bde5363b2f57964c8
NickChen <nickchen14@gamil.com>
2014-07-05 15:57:49 +0800

    get path

--- details.c.php
+++ details.c.php
@@ -4,6 +4,7 @@ $cProduct = new Product();
 $categoriesMenu = $cProduct->getCategoriesMenu();
 $product_id = intval($_GET['i']);
 $product_arr = $cProduct->getProduct($product_id);
+$path = $cProduct->getProductPath($product_arr['table_id'],$product_arr['products_id']);
 //$quantity_visible = $product_arr['quantity_visible'];
 //$color_options = $product_arr['color_options'];
 //$cri_options = $product_arr['cri_options'];
