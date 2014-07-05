7dd6727098c3b7eee71a2b062b2ef9a019a430e4
NickChen <nickchen14@gamil.com>
2014-07-05 16:49:05 +0800

    hash

--- includes/javascript.php
+++ includes/javascript.php
@@ -6,3 +6,14 @@
 	<script src="scripts/jquery.accordion.source.js" type="text/javascript" ></script>
 	<script src="scripts/script.js" type="text/javascript" ></script>
 	<script src="http://localhost:35729/livereload.js"></script>
+    <script type="text/javascript">
+    $(function(){
+        var url = window.location.href.toString();
+        var hashPos = url.search('#');
+        if (hashPos > 0) {
+            var hash = url.split('#')[1];
+            var link = $('a.btn.btn-primary').attr('href');
+            $('a.btn.btn-primary').attr('href',link +'#' +  hash);
+        }
+        });
+    </script>
