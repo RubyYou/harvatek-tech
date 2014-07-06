f42ca614580d7246ac6329ce8b4c7a79b56c15c5
Ruby You <shih@svisualjoint.co.uk>
2014-07-06 15:58:56 +0100

    favicon and keywords - clean header

--- includes/header.php
+++ includes/header.php
@@ -1,22 +1,33 @@
 <!doctype html>
 <head>
+
+<!-- Basic Page Needs
+  ================================================== -->
+
   <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
   <meta name="Author" content="Harvatek-tech.com" />
-  <meta name="Keywords" content=""/>
-  <meta name="Description" content="" />
+  <meta name="Keywords" content="Harvatek, LED products, lighten technology, Display, SMD LED, Infrared Emitter / Sensor / Coupler, Through Hole LED, UV LED"/>
+  <meta name="Description" content="Harvatek Technologies is the resources of a world class LED manufacturer based in Hsin-Chu, Taiwan. Designing components to meet the needs of a wide range of design and efficiency requirement, Harvatek Technologies offers high quality and competitive LED solutions for industries across the globe." />
   <meta http-equiv="Content-Script-Type" content="text/javascript" />
 
-  <!--mobile setup // this is todo
+<!-- Mobile Specific Metas
+  ================================================== -->
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="apple-mobile-web-app-capable" content="yes" />
 
-  <link rel="apple-touch-icon" href="images/iphone-57.png"/>
-  <link rel="icon" href="images/ulacicon.ico" type="image/ico" />
-	--> 
+<!-- CSS
+  ================================================== -->    
   <link rel="stylesheet" type="text/css" media="screen" href="scripts/bootstrap/bootstrap.css" />
   <link rel="stylesheet" href="http://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css" >
   <link rel="stylesheet" type="text/css" media="screen" href="scripts/css/style.css" />
-
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <meta name="apple-mobile-web-app-capable" content="yes" />
+  
+<!-- Favicons
+  ================================================== -->
+  <link rel="shortcut icon" href="images/favicon.ico">
+  <link rel="apple-touch-icon" href="images/apple-touch-icon.png">
+  <link rel="apple-touch-icon" sizes="72x72" href="images/apple-touch-icon-72x72.png">
+  <link rel="apple-touch-icon" sizes="114x114" href="images/apple-touch-icon-114x114.png">
+  
   <title>
     Harvatek-tech | Lighting your future
   </title>
