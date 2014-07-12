6f931de6444987422dc52e8ce946e976ff0e0e0f
Ruby You <shih@svisualjoint.co.uk>
2014-07-12 05:32:18 +0100

    update new info for local representatives

--- local-representatives.php
+++ local-representatives.php
@@ -33,8 +33,9 @@
 ================================================== -->
 
 <!-- todo: need to take out iframe: or change to other way of doing / not responsive and can not be hover as well-->
-<iframe id="mapFrame" class="visible-lg visible-md" src="http://www.harvatek-tech.com/salesMap.html" 
-		height="642" width="1000" frameborder="0" scrolling="no"></iframe>
+<iframe id="mapFrame" class="visible-lg visible-md" src="salesMap.html" 
+		height="642" width="1028" frameborder="0" scrolling="no"></iframe>
+<br/><br/>
 <img class="visible-sm visible-xs img-responsive" src="http://harvatek-tech.com/wp-content/uploads/2013/08/salesMap.jpg">
 <div class="row salesRepo">
 	<ul class="col-sm-4">
@@ -63,7 +64,7 @@
 		<li><a href="http://www.ccrep.com" target="_blank">www.ccrep.com</a></li>
 		<li></li>
 	</ul>
-	</div>
+</div>
 <div class="row salesRepo">
 	<ul class="col-sm-4">
 		<li style="color: #5ddff4;">TRINITY</li>
@@ -82,6 +83,35 @@
 		<li><a href="http://www.a-t-m-i.com" target="_blank">www.a-t-m-i.com</a></li>
 	</ul>
 	<ul class="col-sm-4">
+		<li style="color: #83dd9f;">Dyn-e-Mark</li>
+		<li>Florida, Puerto Rico, North Carolina, South Carolina, Tennessee, Georgia
+			Alabama, Mississippi</li>
+		<li><i class="fa fa-phone"></i> 407-660-1661</li>
+		<li><a href="http://www.dyneamark.com" target="_blank">www.dyneamark.com</a></li>
+	</ul>
+</div>
+<div class="row salesRepo">
+	<ul class="col-sm-4">
+		<li style="color:#b299f2;">Eagle Ridge Mktg</li>
+		<li>CO, UT</li>
+		<li><i class="fa fa-phone"></i> 303-667-2699</li>
+	</ul>
+	<ul class="col-sm-4">
+		<li style="color:#938eff;">Tech Coast Sales</li>
+		<li>Southwest, Az and NM</li>
+		<li><a href="mailto:sales@tc-sales.com">sales@tc-sales.com</a></li>
+		<li><i class="fa fa-phone"></i> 949-305-6869</li>
+	</ul>
+	<ul class="col-sm-4">
+		<li style="color:#054a51;">KOBAMA</li>
+		<li>Brazil</li>
+		<li><i class="fa fa-phone"></i>+55 11 5102-2460</li>
+		<li><i class="fa fa-print"></i>+55 11 5102-2460</li>
+		<li><a href="http://kobama.com.br" target="_blank">kobama.com.br</a></li>
+	</ul>
+</div>
+<div class="row salesRepo">
+	<ul class="col-sm-4">
 		<li style="color: #468dcb;">HARVATEK Technology</li>
 		<li>The USA, Canada, Mexico</li>
 		<li><a href="mailto:sales@harvatek-tech.com">sales@harvatek-tech.com</a></li>
@@ -89,7 +119,12 @@
 		<li><i class="fa fa-print"></i> 408-844-9618</li>
 		<li><a href="www.harvatek-tech.com">www.harvatek-tech.com</a></li>
 	</ul>
-	</div>
+</div>
+
+
+
+
+
 	</div>
 </section>
 <div id='layout_footer'></div>
